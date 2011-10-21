d6fb6ca1f8e0e964240bbfcfe655dad9f8d5ca8c
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-10-20 23:02:01 -0600

    Proper image shuffling

--- Unit 7/sortedImages.php
+++ Unit 7/sortedImages.php
@@ -12,18 +12,11 @@
       }   
     }
     closedir($handle); // We're not using it anymore
-    mt_srand((double)microtime()*1000000); // seed for PHP < 4.2
     
-    $rand_array = array();
-    $outputString = "";
-    $i--;
-    for( $j=0; $j<$number_images;  ) {
-      $rand = mt_rand(0, $i); // $i was incremented as we went along
-      if( !(in_array($rand,$rand_array))) {
-        $outputString .= '<img src="images/random/' . $files[$rand] . '" height="100px" />';
-        $rand_array[$j] = $rand;
-        $j++;
-      }
+    shuffle($files);
+    
+    for( $j=0; $j<$number_images; $j++) {
+      $outputString .= '<img src="images/random/' . $files[$j] . '" height="100px" />';
     }
     
     echo $outputString;
