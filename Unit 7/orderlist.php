6662e214abeaff3efc6e0fdb6df7c3d98a6567ff
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-10-20 18:48:42 -0600

    Cleaning stuff up before merge

--- Unit 7/orderlist.php
+++ Unit 7/orderlist.php
@@ -13,7 +13,6 @@
       $orders = array();
       
       for($i = 0; $input = fgetcsv($fp, 999, "\t"); $i++) {
-        var_dump($input);
         $orders[$i]['date'] = $input[0];
         for($j = 1; $input[$j] != ""; $j++) {
           $itemName = $input[$j];
@@ -31,8 +30,6 @@
       echo '<ol>';
       for($i = 0; $i < count($orders); $i++) {
         foreach($orders[$i] as $item => $row) {
-          ?><li><?var_dump($item);?></li><?
-          ?><li><?var_dump($row);?></li><?
           if($item != 'date') {
             $outputString .= $orders[$i][$item]['qty'] . ' ';
             if($orders[$i][$item]['size'] != NULL) $outputString .= $orders[$i][$item]['size'] . ' ';
@@ -41,30 +38,8 @@
         }
         echo '<li>' . $outputString . '</li>';
       }
-      echo '</0l>';
-      
-      $i = 0;
-      foreach($orders as $order) {
-        echo $i++;
-        $j = 55;
-        foreach($order as $item) {
-          echo $j++;
-        }
-      }
-    
-      var_dump($orders);
-    
-      /*$order = fgets($fp, 999);
-      if( $order == "" ) {
-        ?><p style="font-weight: bold;">No orders pending, try again some other time.</p><?php
-      } else {
-        echo $order . "<br />";
-      }
-      
-      while(!feof($fp)) {
-        $order = fgets($fp, 999);
-        echo $order . "<br />";
-      }*/
+      echo '</ol>';
+
       
       fclose($fp);
     ?>
