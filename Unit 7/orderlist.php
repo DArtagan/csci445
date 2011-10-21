6ae711c8a3ffe8b682dd7187d08528f43d4c19f4
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-10-20 20:11:33 -0600

    Now we're good.  Merging now.

--- Unit 7/orderlist.php
+++ Unit 7/orderlist.php
@@ -1,8 +1,10 @@
 <html>
   <head>
     <title>Order List</title>
+    <?php require('reference.php'); ?>
   </head>
   <body>
+    <h1>Order List</h1>
     <?php
       @ $fp = fopen("orders.txt", 'rb');
       if(!$fp) {
@@ -13,8 +15,9 @@
       $orders = array();
       
       for($i = 0; $input = fgetcsv($fp, 999, "\t"); $i++) {
-        $orders[$i]['date'] = $input[0];
-        for($j = 1; $input[$j] != ""; $j++) {
+        $orders[$i]['meta']['date'] = $input[0];
+        $orders[$i]['meta']['name'] = $input[1];
+        for($j = 2; $input[$j] != ""; $j++) {
           $itemName = $input[$j];
           $j++;
           $orders[$i]["$itemName"]['qty'] = $input[$j];
@@ -27,18 +30,30 @@
         }
       }
       
+      $grandtotal = 0;
       echo '<ol>';
       for($i = 0; $i < count($orders); $i++) {
+        $total = 0;
+        $outputString = $orders[$i]['meta']['date'] . '; ';
         foreach($orders[$i] as $item => $row) {
-          if($item != 'date') {
+          if($item != 'meta') {
             $outputString .= $orders[$i][$item]['qty'] . ' ';
             if($orders[$i][$item]['size'] != NULL) $outputString .= $orders[$i][$item]['size'] . ' ';
-            $outputString .= $item . ', ';
+            if($orders[$i][$item]['qty'] == 1) {
+              $outputString .= $item . ', ';
+            } else {
+              $outputString .= $products[$item]['plural'] . ', ';
+            }
+            $total += ($products[$item]['price'] * $orders[$i][$item]['qty']);
           }
         }
+        $outputString = substr($outputString,0,-2) . '; ' . '$' . number_format($total, 2);
+        $orders[$i]['meta']['total'] = $total;
+        $grandtotal += $total;
         echo '<li>' . $outputString . '</li>';
       }
       echo '</ol>';
+      echo '<p>Grandtotal: $' . number_format($grandtotal, 2) . '</p>';
 
       
       fclose($fp);
